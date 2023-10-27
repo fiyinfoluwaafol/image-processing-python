81061b419f29d569080777988b4ac938ca7809b3
Fiyinfoluwa Afolayan <fiyinfoluwa.afolayan@gmail.com>
2023-10-27 19:05:56 -0400

    Refined code to specify size of box

--- custom.py
+++ custom.py
@@ -5,15 +5,15 @@ def polka_box(image_matrix):
     width = len(image_matrix[0])
     center_row = height // 2
     center_col = width // 2
-
-    
-    for i in range(random.randint(1,40)):
-        center_row = random.randint(25,height-24)
-        center_col = random.randint(25,width -24)
+    size_of_dot = 10
+    #random.randint(50,70)
+    for i in range(random.randint(100,150)):
+        center_row = random.randint((size_of_dot//2),height-(size_of_dot//2)-1)
+        center_col = random.randint((size_of_dot//2),width -(size_of_dot//2)-1)
     
         rand_color = (random.randint(0,255),random.randint(0,255),random.randint(0,255))
-        for row in range(center_row - 25, center_row + 25):
-            for col in range(center_col - 25, center_col + 25):
+        for row in range(center_row - (size_of_dot//2), center_row + (size_of_dot//2)):
+            for col in range(center_col - (size_of_dot//2), center_col + (size_of_dot//2)):
                 image_matrix[row][col] = rand_color
     return image_matrix
 
